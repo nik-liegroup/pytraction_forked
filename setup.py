584c68d8ddce69d3f4ec7847158f008d222f4479
nik-liegroup <74056186+nik-liegroup@users.noreply.github.com>
2024-04-21 20:22:40 +0200

    removed Cython in setup.py due to installation error

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy as np
+#from Cython.Build import cythonize
+#import numpy as np
 
 VERSION = "0.1.2"
 
@@ -18,7 +18,7 @@ REQUIREMENTS = [
     "attrs==23.2.0",
     "certifi==2024.2.2",
     "cycler==0.12.1",
-    "Cython==3.0.10",
+    "Cython~=3.0.10",
     "decorator==5.1.1",
     "gitdb==4.0.11",
     "GitPython==3.1.43",
@@ -56,12 +56,12 @@ REQUIREMENTS = [
     "tqdm==4.66.2",
 ]
 
-ext_modules = [Extension(name="pytraction.widim",
-                         sources=["pytraction/widim.pyx"],
-                         include_dirs=[np.get_include()])]
+#ext_modules = [Extension(name="pytraction.widim",
+#                         sources=["pytraction/widim.pyx"],
+#                         include_dirs=[np.get_include()])]
 
 
-SETUP_REQUIRES = ["pytest-cov", "pytest-runner", "pytest", "codecov", "Cython", "numpy"]
+SETUP_REQUIRES = ["pytest-cov", "pytest-runner", "pytest", "codecov"]
 TESTS_REQUIRES = ["pytest-cov", "codecov"]
 
 
@@ -79,7 +79,7 @@ options = {
     "description": DESCRIPTION,
     "classifiers": CLASSIFIERS,
     "packages": PACKAGES,
-    "ext_modules": cythonize(ext_modules),
+    #"ext_modules": cythonize(ext_modules),
     "include_package_data": True,
     "package_data": {'pytraction': ['*.pth', '*.pickle', '.pyx', '*.pyd', '*.so']},
     "setup_requires": SETUP_REQUIRES,
